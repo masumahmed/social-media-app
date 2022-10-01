add260dc79fbb2f91f3147803d647e3542bf634d
Masum Ahmed <masumahmed1798@gmail.com>
2022-10-01 06:03:43 -0400

    login

--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -2,23 +2,27 @@ import './Login.css'
 import { Link } from 'react-router-dom'
 
 export default function Login() {
-    return <div class="content">
-        <div class="flex-div">
-            <div class="name-content">
-                <h1 class="logo">SocialMediaApp</h1>
-                <p>Connect with friends and the world around you on social media.</p>
+    document.getElementsByTagName('body').classList += 'wave';
+
+    return <div className="waveWrapper">
+        <div class="content">
+            <div class="flex-div">
+                <div class="name-content">
+                    <h1 class="logo">SocialMediaApp</h1>
+                    <p>Connect with friends and the world around you on social media.</p>
+                </div>
+                <form>
+                    <h1>Welcome</h1>
+                    <br></br>
+                    <input type="text" placeholder="Email or Phone Number" required />
+                    <input type="password" placeholder="Password" required />
+                    <button class="login">Log In</button>
+                    <br></br>
+                    <p class="">Forgot Password?</p>
+                    <hr></hr>
+                    <Link to='#' class="create-account">Create Account</Link>
+                </ form>
             </div>
-            <form>
-                <h1>Login</h1>
-                <br></br>
-                <input type="text" placeholder="Email or Phone Number" required />
-                <input type="password" placeholder="Password" required />
-                <button class="login">Log In</button>
-                <br></br>
-                <p class="">Forgot Password?</p>
-                <hr></hr>
-                <Link to='#' class="create-account">Create Account</Link>
-            </ form>
         </div>
     </div>
 }
